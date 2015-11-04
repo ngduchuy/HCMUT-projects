aee27d783eeb21dfe7811602ef30a7e9b9cc139a
Nguyen Duc Tho <ndtho8205@gmail.com>
2015-11-04 13:31:47 +0700

    Update project

--- roll-and-roll/Assets/Scripts/CloudController.cs
+++ roll-and-roll/Assets/Scripts/CloudController.cs
@@ -5,11 +5,11 @@ public class CloudController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.position += new Vector3 ((float)0.5* Time.deltaTime, 0, 0) ;
 	}
 }
--- roll-and-roll/Assets/Scripts/PlayerController.cs
+++ roll-and-roll/Assets/Scripts/PlayerController.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	private Rigidbody2D rb;
+	Vector3 movement;
 
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody2D>();
+		movement = this.transform.position;
+		movement = new Vector3(1f, 0, 1);
+		rb.AddForce (movement * speed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//transform.Rotate( 0.0f, 0.0f,0.1f*Time.deltaTime);
+
+		
 	}
 }
